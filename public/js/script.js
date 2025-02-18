2d6374c227949a54f48f59128c461177ab96d3ec
jn-panasonic <james.nguyen@sg.panasonic.com>
2025-02-18 14:44:34 +0800

    reformat the html

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-document.getElementById("send-button").addEventListener("click", function () { sendMessage() });
+//document.getElementById("send-button").addEventListener("click", function () { sendMessage() });
 
-document.getElementById("speak-button-container").addEventListener("click", function () { startRecognize() });
+//document.getElementById("speak-button-container").addEventListener("click", function () { startRecognize() });
 
-document.getElementById("listening-button-container").addEventListener("click", function () { endRecognize(false) });
+//document.getElementById("listening-button-container").addEventListener("click", function () { endRecognize(false) });

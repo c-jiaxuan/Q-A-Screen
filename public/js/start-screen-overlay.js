4ca6d43ee318e672beb34dafc08b49a1ecdbee2b
cjixuan <jiaxuan.cheng@sg.panasonic.com>
2025-02-24 10:57:47 +0800

    start button

---
+++ public/js/start-screen-overlay.js
@@ -0,0 +1,5 @@
+document.getElementById("start-button").addEventListener("click", function() {
+    // Hide overlay
+    document.getElementById("overlay").style.display = "none";
+    //speak(botMessages["start_msg"][0].message, botMessages["start_msg"][0].gesture, false);
+});
